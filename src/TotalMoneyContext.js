1252d6a49993083b815ccd82dde72cb864364c63
Maliha Tanweer <malihatanweer@gmail.com>
2025-03-19 12:11:25 -0400

    Converted total amount as int

--- src/TotalMoneyContext.js
+++ src/TotalMoneyContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const TotalMoneyContext = createContext();
 
 export const TotalMoneyProvider = ({children}) => {
-    const [submittedIncome, setSubmittedIncome] = useState(""); // State for displaying submitted income
+    const [submittedIncome, setSubmittedIncome] = useState(); // State for displaying submitted income
 
     const addTotal = async (total) => {
         setSubmittedIncome(total);
